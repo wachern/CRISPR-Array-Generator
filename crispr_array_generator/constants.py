0fb74b116add85cd69f15f2f9310cef145ecf22e
wachern <147002077+wachern@users.noreply.github.com>
2023-12-02 10:33:26 -0800

    Update constants.py

--- crispr_array_generator/constants.py
+++ crispr_array_generator/constants.py
@@ -1,3 +1,6 @@
 """Constants used in module."""
 
-BASE_PAIRS = {'A' : 'T', 'C' : 'G', 'G' : 'C', 'T' : 'A', 't' : 'a', 'c' : 'g', 'g' : 'c', 'a' : 't'}
+BASE_PAIRS = {'A' : 'T', 'C' : 'G', 'G' :
+              'C', 'T' : 'A', 't' : 'a', 'c' :
+              'g', 'g' : 'c', 'a' : 't'}
+VALID_DNA = "ACGTacgt"
