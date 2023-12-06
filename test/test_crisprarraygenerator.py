c5d16ffbd82c2b52708d196521a05209349b99b4
wachern <147002077+wachern@users.noreply.github.com>
2023-12-05 17:44:22 -0800

    Update and rename test_crisprarraygenerator to test_crisprarraygenerator.py

---
+++ test/test_crisprarraygenerator.py
@@ -0,0 +1,18 @@
+"""
+Created: Fall 2023
+Author: Willow Chernoske
+"""
+
+import unitest
+from from crispr_array_generator.crisprarraygenerator import Array
+
+class TestArray(unittest.TestCase):
+
+    def setUp(self):
+        forward = atgcgga
+        reverse = tccgcat
+
+    def test_get_reverse_complement(self)
+        result = self.forward.get_reverse_complement()
+        self.assertIsNotNone(result)
+        self.assertEqual(reverse)
