00aa57ac58dce007181826d4b13caec9e5a3305b
wachern <147002077+wachern@users.noreply.github.com>
2023-12-10 20:59:22 -0800

    Update constants.py

--- crispr_array_generator/constants.py
+++ crispr_array_generator/constants.py
@@ -7,4 +7,5 @@ Author: Willow Chernoske
 BASE_PAIRS = {'A' : 'T', 'C' : 'G', 'G' :
               'C', 'T' : 'A', 't' : 'a', 'c' :
               'g', 'g' : 'c', 'a' : 't'}
+
 VALID_DNA = "ACGTacgt"
