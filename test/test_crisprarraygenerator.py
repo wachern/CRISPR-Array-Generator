226d209eb8d743876d9d701fdf99f683b8677244
wachern <147002077+wachern@users.noreply.github.com>
2023-12-10 17:38:52 -0800

    Update test_crisprarraygenerator.py

--- test/test_crisprarraygenerator.py
+++ test/test_crisprarraygenerator.py
@@ -1,4 +1,5 @@
 """
+Test cases for crispr_array_generator
 Created: Fall 2023
 Author: Willow Chernoske
 """
@@ -9,10 +10,22 @@ from crispr_array_generator.crisprarraygenerator import Array
 class TestArray(unittest.TestCase):
 
     def setUp(self):
-        forward = atgcgga
-        reverse = tccgcat
+        #forward and reverse
+        self.forward = 'atgcgga'
+        self.reverse = 'tccgcat'
+
+        #removes TCC and non-nucleotide grnas
+        self.grnas_input = ['ttcaaaggg', 'cccccc', 'tchtaa']
+        self.grnas_output = ['aaaggg', 'cccccc']
 
     def test_get_reverse_complement(self):
-        result = self.forward.get_reverse_complement()
+        result = Array.get_reverse_complement(self.forward)
         self.assertIsNotNone(result)
-        self.assertEqual(reverse)
+        self.assertEqual(result, self.reverse)
+        print("OK!")
+
+    def test_check_grna(self):
+        result = Array.check_grna(self.grnas_input)
+        self.assertIsNotNone(result)
+        self.assertEqual(result, self.grnas_output)
+        print("OK2!")
